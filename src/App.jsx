b691404cb4efdf64f5057fa05c9e64109f118025
Brajesh <281092322419e@gmail.com>
2025-07-21 16:46:56 +0530

    completed number selection func.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import GamePage from "./components/GamePage";
 import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/game" element={<GamePage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import dices from "../assets/dices.png";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleRoute = () => {
+    navigate("/game");
+  };
+
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="flex">
@@ -13,7 +20,10 @@ const HomePage = () => {
         {/* Right Half: Content */}
         <div className="w-1/2 flex flex-col justify-center items-center text-black p-6">
           <h1 className="text-6xl font-bold mb-6">DICE GAME</h1>
-          <button className="bg-black text-white font-semibold ml-[200px] px-8 py-2 rounded hover:bg-gray-800 transition duration-300">
+          <button
+            className="cursor-pointer bg-black text-white font-semibold ml-[200px] px-8 py-2 rounded hover:bg-gray-800 transition duration-300"
+            onClick={handleRoute}
+          >
             Play Now
           </button>
         </div>
